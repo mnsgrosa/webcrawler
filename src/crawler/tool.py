4b146a5861cf534d1dabda0ac37fdb2880d46213
Matheus Rosa <matheusnsampaio@gmail.com>
2025-08-12 06:18:21 -0300

    :hammer: Adjusting to the correct tool path adding debugging and other changes to the tool: <TODO: place in airflow dir in next commits>

--- src/crawler/tool.py
+++ src/crawler/tool.py
@@ -6,7 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
-from logger import MyLogger
+from src.utils.logger import MyLogger
+from datetime import datetime
+import httpx
 import json
 import time
 
@@ -29,7 +31,7 @@ class MyCrawler(MyLogger):
         self.driver.get(self.url)
         self.logger.info('Base page entered')
 
-        self.games_list = {}
+        self.games_list = []
 
     def get_all_deals_page(self):
         if self.platform == 'playstation':
@@ -67,6 +69,7 @@ class MyCrawler(MyLogger):
                 titles = games.find_all('li')
                 self.logger.info(f'Found {len(titles)} titles')
                 self.logger.info(f'Type of titles: {type(titles)}')
+                date = datetime.now().date()
                 for title in titles:
                     self.logger.info(f'Title: {title.get_text(strip = True)}')
                     game_name = title.find('span', {'data-qa': lambda x: x and 'product-name' in x}).get_text(strip = True)
@@ -75,15 +78,28 @@ class MyCrawler(MyLogger):
                     self.logger.info(f'Game type: {game_type}')
                     discount_price = title.find('span', {'data-qa': lambda x: x and 'price' in x}).get_text(strip = True)
                     self.logger.info(f'Discount price: {discount_price}')
-    
-                    self.logger.info(f'Starting to scrape platforms')
-                    platforms = [tag.get_text(strip=True) for tag in title.find_all('span', class_='psw-platform-tag')]
-                    self.logger.info(f'Platforms: {platforms}')
-                    self.games_list[game_name] = {
+                    self.games_list.append({
+                        'date': date,
+                        'platform': self.platform,
+                        'game_name': game_name,
                         'game_type': game_type,
-                        'discount_price': discount_price,
-                        'platforms': platforms
-                    }
+                        'price': discount_price
+                    })
                     return self.games_list
             except Exception as e:
                 self.logger.error(f'Couldnt get deals:{e}')
+
+    def post_contents(self):
+        self.logger.info('Starting post method')
+        if self.game_list is None:
+            self.logger.info('Get the deals first')
+            return {}
+        try:
+            self.logger.info('Starting to post the deals')
+            with httpx.Client() as client:
+                response = client.post('http://localhost:8000/post/games', json = self.games_list)
+                response.raise_for_status()
+            self.logger.info(f"Deals posted: {response.json().get('status')}")
+            return response.json()
+        except Exception as e:
+            self.logger.error(f'Failed to post deals: {e}')
