5494db3267c170399067097a489db8b8cce79a75
Matheus Rosa <matheusnsampaio@gmail.com>
2025-08-12 06:03:42 -0300

    :sparkles: first api changes

--- src/api/rest.py
+++ src/api/rest.py
@@ -7,12 +7,17 @@ app = FastAPI()
 
 @app.post('/post/games', response_model = StatusMessage)
 def post_games(game_list: GameList):
-    pass
+    columns =  game_list.items[0].model_fields
+    db.upsert_data(game_list.items, columns.items)
+    return {'status': True}
 
-@app.get('/get/all', response_model = GameList)
+@app.get('/get/all')
 def get_games():
-    pass
+    data = db.get_all_data()
+    return {'data': data}
+
 
 @app.get('/get/platform', response_model = GameList)
 def get_games_platform(request: PlatformRequest):
-    pass
+    data = db.get_specific_games(request.platform, request.date)
+    return {'items': data}
