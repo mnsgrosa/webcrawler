c725254be84d2fee731a5b3cb2087437f73d5e10
Matheus Rosa <matheusnsampaio@gmail.com>
2025-08-12 05:42:20 -0300

    :sparkles: adding db initialization script

---
+++ init_db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 <<-EOSQL
+    CREATE USER psql WITH ENCRYPTED PASSWORD 'my_password';
+    CREATE DATABASE deals_db OWNER psql;
+    \connect psql;
+    CREATE SCHEMA deals_schema AUTHORIZATION psql;
+
+    CREATE TABLE deals_schema.deals(
+        date DATE DEFAULT CURRENT_DATE,
+        console VARCHAR(4),
+        game_name VARCHAR(255),
+        game_type VARCHAR(255),
+        price NUMERIC(2)
+    );
+
+    GRANT ALL PRIVILEGES ON SCHEMA deals_schema TO psql;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA deals_schema TO psql;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA deals_schema TO psql;
+EQOSQL
